bff73e90703056742a2c45e1907b22b6d2dd0659
Abdelmageed23 <abd.abdelmageed@gmail.com>
2021-12-26 15:21:47 +0200

    XO V1.1 comments added

--- XO.c
+++ XO.c
@@ -1,13 +1,23 @@
+/******************************************/
+/***********Date: 26.12.2021***************/
+/**********Project Name: X-O Game**********/
+/******Author: Abdelrahman Mohamed ********/
+/************* Version: 1.1 ***************/
+/******************************************/
+
+#include <stdint.h>
 #include <conio.h>
 
-char arr[9]={' ',' ',' ',' ',' ',' ',' ',' ',' '};
-char turn[] = {'X','O'};
-int player_turn=0;
+/* Initializing Global variable */
+uint8_t gu8arr_arr[9]={' ',' ',' ',' ',' ',' ',' ',' ',' '};
+uint8_t gu8arr_turn[] = {'X','O'};
+uint8_t gu8_player_turn=0;
 
+/*Declaration of some functions*/
 void game_Instrction();
 void pattern();
-char First_player();
-int check_winner();
+uint8_t First_player();
+uint8_t check_winner();
 void play_XO();
 
 void main()
@@ -17,25 +27,27 @@ void main()
 }
 
 /*
-pattern function to print the X-O pattern
+pattern function to the print the Game Instruction for first time
 */
 void game_Instrction()
 {
     printf("welcome to X-O Game\n");
     printf("select the number you want to add as the pattern\n");
     printf("\n%d|%d|%d\n%d|%d|%d\n%d|%d|%d\n\n",1,2,3,4,5,6,7,8,9);
-
 }
+
+/* function to print the updated function  */
 void pattern(void)
 {
-    printf("\n%c|%c|%c\n%c|%c|%c\n%c|%c|%c\n\n",arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8]);
+    printf("\n%c|%c|%c\n%c|%c|%c\n%c|%c|%c\n\n",gu8arr_arr[0],gu8arr_arr[1],gu8arr_arr[2],gu8arr_arr[3],gu8arr_arr[4],gu8arr_arr[5],gu8arr_arr[6],gu8arr_arr[7],gu8arr_arr[8]);
 }
+
 /*
 First player function to select who will play first X or O and return 0 for X player and 1 for O player
 */
-char First_player()
+uint8_t First_player()
 {
-    char player;
+    uint8_t player;
     printf("Select player to play first X or O\n");
     while(1)
     {
@@ -56,15 +68,16 @@ char First_player()
     }
 }
 
+/* the game function, will control all the program */
 void play_XO()
 {
-    game_Instrction();
-    player_turn = First_player(); // to adjust the turn based on the first player
+    game_Instrction();        // print the instruction first
+    gu8_player_turn = First_player(); // to adjust the turn based on the first player
 
     while(1)
     {
     printf("select number from 1 to 9\n");
-    int input;
+    uint8_t input;
     scanf("%d",&input);
     /*
         check the input selection from 1 to 9
@@ -72,13 +85,13 @@ void play_XO()
     switch(input)
     {
     case 1:
-        if(arr[0]==' ')
+        if(gu8arr_arr[0]==' ')
         {
             /*
                 adjust the selection by reminder operation as if the turn divisible by 2 so its X turn else O turn
             */
-            arr[0]=turn[player_turn%2];
-            player_turn++;
+            gu8arr_arr[0]=gu8arr_turn[gu8_player_turn%2];
+            gu8_player_turn++;
             pattern();
             if (check_winner()==0)  return 0;
         }
@@ -86,10 +99,10 @@ void play_XO()
             printf("busy location, select another one ");
             break;
     case 2:
-        if(arr[1]==' ')
+        if(gu8arr_arr[1]==' ')
         {
-            arr[1]=turn[player_turn%2];
-            player_turn++;
+            gu8arr_arr[1]=gu8arr_turn[gu8_player_turn%2];
+            gu8_player_turn++;
             pattern();
             if (check_winner()==0)  return 0;
         }
@@ -97,10 +110,10 @@ void play_XO()
             printf("busy location, select another one ");
              break;
     case 3:
-        if(arr[2]==' ')
+        if(gu8arr_arr[2]==' ')
         {
-            arr[2]=turn[player_turn%2];
-            player_turn++;
+            gu8arr_arr[2]=gu8arr_turn[gu8_player_turn%2];
+            gu8_player_turn++;
             pattern();
             if (check_winner()==0)  return 0;
 
@@ -109,10 +122,10 @@ void play_XO()
             printf("busy location, select another one ");
              break;
     case 4:
-        if(arr[3]==' ')
+        if(gu8arr_arr[3]==' ')
         {
-            arr[3]=turn[player_turn%2];
-            player_turn++;
+            gu8arr_arr[3]=gu8arr_turn[gu8_player_turn%2];
+            gu8_player_turn++;
             pattern();
             if (check_winner()==0)  return 0;
 
@@ -121,10 +134,10 @@ void play_XO()
             printf("busy location, select another one ");
              break;
     case 5:
-        if(arr[4]==' ')
+        if(gu8arr_arr[4]==' ')
         {
-            arr[4]=turn[player_turn%2];
-            player_turn++;
+            gu8arr_arr[4]=gu8arr_turn[gu8_player_turn%2];
+            gu8_player_turn++;
             pattern();
             if (check_winner()==0)  return 0;
         }
@@ -132,10 +145,10 @@ void play_XO()
             printf("busy location, select another one ");
              break;
     case 6:
-        if(arr[5]==' ')
+        if(gu8arr_arr[5]==' ')
         {
-            arr[5]=turn[player_turn%2];
-            player_turn++;
+            gu8arr_arr[5]=gu8arr_turn[gu8_player_turn%2];
+            gu8_player_turn++;
             pattern();
            if (check_winner()==0)  return 0;
         }
@@ -144,10 +157,10 @@ void play_XO()
              break;
 
     case 7:
-        if(arr[6]==' ')
+        if(gu8arr_arr[6]==' ')
         {
-            arr[6]=turn[player_turn%2];
-            player_turn++;
+            gu8arr_arr[6]=gu8arr_turn[gu8_player_turn%2];
+            gu8_player_turn++;
             pattern();
             if (check_winner()==0)  return 0;
         }
@@ -155,10 +168,10 @@ void play_XO()
             printf("busy location, select another one ");
              break;
     case 8:
-        if(arr[7]==' ')
+        if(gu8arr_arr[7]==' ')
         {
-            arr[7]=turn[player_turn%2];
-            player_turn++;
+            gu8arr_arr[7]=gu8arr_turn[gu8_player_turn%2];
+            gu8_player_turn++;
             pattern();
             if (check_winner()==0)  return 0;
         }
@@ -166,10 +179,10 @@ void play_XO()
             printf("busy location, select another one ");
              break;
     case 9:
-        if(arr[8]==' ')
+        if(gu8arr_arr[8]==' ')
         {
-            arr[8]=turn[player_turn%2];
-            player_turn++;
+            gu8arr_arr[8]=gu8arr_turn[gu8_player_turn%2];
+            gu8_player_turn++;
             pattern();
             if (check_winner()==0)  return 0;
 
@@ -183,50 +196,54 @@ void play_XO()
 }
 
 /*
-check the winner X or O by:
-it the 3 row or 3 column or any diagonal is equal so he is the winner
-
-check if the pattern is full gives GAME OVER
+check the winner X or O or game over and return 0 if the game ends
 */
-int check_winner()
+uint8_t check_winner()
 {
-    if((arr[0]==arr[1]&&arr[1]==arr[2]&&arr[2]=='X')||(arr[3]==arr[4]&&arr[4]==arr[5]&&arr[5]=='X')||(arr[6]==arr[7]&&arr[7]==arr[8]&&arr[8]=='X'))
+    /* check the 3 rows if equal X is the winner */
+    if((gu8arr_arr[0]==gu8arr_arr[1]&&gu8arr_arr[1]==gu8arr_arr[2]&&gu8arr_arr[2]=='X')||(gu8arr_arr[3]==gu8arr_arr[4]&&gu8arr_arr[4]==gu8arr_arr[5]&&gu8arr_arr[5]=='X')||(gu8arr_arr[6]==gu8arr_arr[7]&&gu8arr_arr[7]==gu8arr_arr[8]&&gu8arr_arr[8]=='X'))
     {
         printf("\n*********X-player win*********\n");
         return 0;
     }
 
-    else if((arr[0]==arr[3]&&arr[3]==arr[6]&&arr[6]=='X')||(arr[1]==arr[4]&&arr[4]==arr[7]&&arr[7]=='X')||(arr[2]==arr[5]&&arr[5]==arr[8]&&arr[8]=='X'))
+    /* check the 3 columns if equal X is the winner */
+    else if((gu8arr_arr[0]==gu8arr_arr[3]&&gu8arr_arr[3]==gu8arr_arr[6]&&gu8arr_arr[6]=='X')||(gu8arr_arr[1]==gu8arr_arr[4]&&gu8arr_arr[4]==gu8arr_arr[7]&&gu8arr_arr[7]=='X')||(gu8arr_arr[2]==gu8arr_arr[5]&&gu8arr_arr[5]==gu8arr_arr[8]&&gu8arr_arr[8]=='X'))
     {
         printf("\n*********X-player win*********\n");
         return 0;
     }
 
-    else if((arr[0]==arr[4]&&arr[4]==arr[8]&&arr[8]=='X')||(arr[2]==arr[4]&&arr[4]==arr[6]&&arr[6]=='X'))
+    /* check the 2 diagonals if equal X is the winner */
+    else if((gu8arr_arr[0]==gu8arr_arr[4]&&gu8arr_arr[4]==gu8arr_arr[8]&&gu8arr_arr[8]=='X')||(gu8arr_arr[2]==gu8arr_arr[4]&&gu8arr_arr[4]==gu8arr_arr[6]&&gu8arr_arr[6]=='X'))
     {
         printf("\n*********X-player win*********\n");
         return 0;
     }
 
-    else if((arr[0]==arr[1]&&arr[1]==arr[2]&&arr[2]=='O')||(arr[3]==arr[4]&&arr[4]==arr[5]&&arr[5]=='O')||(arr[6]==arr[7]&&arr[7]==arr[8]&&arr[8]=='O'))
+    /* check the 3 rows if equal O is the winner */
+    else if((gu8arr_arr[0]==gu8arr_arr[1]&&gu8arr_arr[1]==gu8arr_arr[2]&&gu8arr_arr[2]=='O')||(gu8arr_arr[3]==gu8arr_arr[4]&&gu8arr_arr[4]==gu8arr_arr[5]&&gu8arr_arr[5]=='O')||(gu8arr_arr[6]==gu8arr_arr[7]&&gu8arr_arr[7]==gu8arr_arr[8]&&gu8arr_arr[8]=='O'))
     {
         printf("\n*********O-player win*********\n");
         return 0;
     }
 
-    else if((arr[0]==arr[3]&&arr[3]==arr[6]&&arr[6]=='O')||(arr[1]==arr[4]&&arr[4]==arr[7]&&arr[7]=='O')||(arr[2]==arr[5]&&arr[5]==arr[8]&&arr[8]=='O'))
+    /* check the 3 columns if equal O is the winner */
+    else if((gu8arr_arr[0]==gu8arr_arr[3]&&gu8arr_arr[3]==gu8arr_arr[6]&&gu8arr_arr[6]=='O')||(gu8arr_arr[1]==gu8arr_arr[4]&&gu8arr_arr[4]==gu8arr_arr[7]&&gu8arr_arr[7]=='O')||(gu8arr_arr[2]==gu8arr_arr[5]&&gu8arr_arr[5]==gu8arr_arr[8]&&gu8arr_arr[8]=='O'))
     {
         printf("\n*********O-player win*********\n");
         return 0;
     }
 
-    else if((arr[0]==arr[4]&&arr[4]==arr[8]&&arr[8]=='O')||(arr[2]==arr[4]&&arr[4]==arr[6]&&arr[6]=='O'))
+    /* check the 2 diagonals if equal O is the winner */
+    else if((gu8arr_arr[0]==gu8arr_arr[4]&&gu8arr_arr[4]==gu8arr_arr[8]&&gu8arr_arr[8]=='O')||(gu8arr_arr[2]==gu8arr_arr[4]&&gu8arr_arr[4]==gu8arr_arr[6]&&gu8arr_arr[6]=='O'))
     {
         printf("\n*********O-player win*********\n");
         return 0;
     }
 
-    else if(arr[0]!=' '&&arr[1]!=' '&&arr[2]!=' '&&arr[3]!=' '&&arr[4]!=' '&&arr[5]!=' '&&arr[6]!=' '&&arr[7]!=' '&&arr[8]!=' ')
+    /* check the pattern is full then no winner and the game is over*/
+    else if(gu8arr_arr[0]!=' '&&gu8arr_arr[1]!=' '&&gu8arr_arr[2]!=' '&&gu8arr_arr[3]!=' '&&gu8arr_arr[4]!=' '&&gu8arr_arr[5]!=' '&&gu8arr_arr[6]!=' '&&gu8arr_arr[7]!=' '&&gu8arr_arr[8]!=' ')
     {
 
         printf("\n***********GAME OVER************\n");
